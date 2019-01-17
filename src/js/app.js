bba923326f11014add7189cccc12336a58160977
betsyvies <ing.betsyvies@gmail.com>
2019-01-17 13:17:16 -0500

    event execution change

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
+const changeView = hash => {
+  switch (hash) {
+    case '#/' : { return getView('views/home.html') }
+    case '#/catalogo' : { return getView('views/catalogo.html') }
+    case '#/accesorios' : { return getView('views/accesorios.html') }
+    case '#/lugares' : { return getView('views/lugares.html') }
+    default: return console.log('404')
+  }
+}
+
 const getView = route => {
   fetch('http://localhost:5000/' + route)
   .then(response => response.text())
@@ -10,3 +20,5 @@ const showView = html => {
 }
 
 getView('views/catalogo.html')
+
+if (("onhashchange" in window)) window.onhashchange = () => changeView(window.location.hash);
