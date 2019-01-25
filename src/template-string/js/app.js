215357225b024872b0e76b9fb7bd21196fa5a17d
betsyvies <ing.betsyvies@gmail.com>
2019-01-25 17:17:35 -0500

    add new solution

--- src/template-string/js/app.js
+++ src/template-string/js/app.js
@@ -1,4 +1,26 @@
-import { objTemp } from './tempString.js';
+import { objTemp } from './tempString.js'
+
+const changeTmp = (hash) => {
+  if (hash === '#/' || hash === '' || hash === '#') {
+    return viewTmp('#/home');
+  } else if (hash === '#/catalogo' || hash === '#/accesorios' || hash === '#/lugares') {
+    return viewTmp(hash);
+  } else {
+    return viewTmp('#/different');
+  }
+}
+
+const viewTmp = (routers) => {
+  const router = routers.substr(2, routers.length - 2)
+  const container = document.getElementById("container")
+  container.innerHTML = objTemp[router];
+}
+
+window.addEventListener('load', changeTmp(window.location.hash))
+if (("onhashchange" in window)) window.onhashchange = () => changeTmp(window.location.hash)
+
+
+/*import { objTemp } from './tempString.js';
 
 const changeTmp = (hash) => {
   switch (hash) {
@@ -22,4 +44,5 @@ const showTmp = tmp => {
 }
 
 window.addEventListener('load', changeTmp(window.location.hash))
-if (("onhashchange" in window)) window.onhashchange = () => changeTmp(window.location.hash);
+if (("onhashchange" in window)) window.onhashchange = () => changeTmp(window.location.hash)
+;*/
